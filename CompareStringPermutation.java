bd6e4ba14d0db21d17bb13ff5166e25e91c7918c
katermar <katermarc98@gmail.com>
2017-05-16 00:40:52 +0300

    solution to the first assignment

---
+++ CompareStringPermutation.java
@@ -0,0 +1,20 @@
+package com.katermar;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * Created by USER on 5/15/2017.
+ */
+public class CompareStringPermutation {
+
+    public boolean compare(String first, String last) {
+        char[] a = first.toLowerCase().toCharArray();
+        char[] b = last.toLowerCase().toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
