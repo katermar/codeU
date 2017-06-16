dcc357978b6b4952731e0df172fc2d7d87b33364
katermar <katermarc98@gmail.com>
2017-06-16 12:46:31 +0300

    added test for an empty grid

--- assignment3/Dictionary.java
+++ assignment3/Dictionary.java
@@ -1,13 +1,44 @@
 package com.katermar.assignment3;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 /**
  * Created by katermar on 6/8/2017.
  */
 public class Dictionary {
-    private HashSet<String> dictionary = new HashSet<>();
-    private HashSet<String> prefixes = new HashSet<>();
+    private final HashSet<String> dictionary;
+    private final HashSet<String> prefixes;
+
+    public Dictionary(HashSet<String> dictionary, HashSet<String> prefixes) {
+        this.dictionary = dictionary;
+        this.prefixes = prefixes;
+    }
+
+    public Dictionary(String[] dictionary, String[] prefixes) {
+        this.dictionary = new HashSet<>(Arrays.asList(dictionary));
+        this.prefixes = new HashSet<>(Arrays.asList(prefixes));
+    }
+
+    public Dictionary(String[] dictionary) {
+        this.dictionary = new HashSet<>(Arrays.asList(dictionary));
+        this.prefixes = new HashSet<>();
+        for (String word : dictionary) {
+            for (int i = 0; i < word.length(); i++) {
+                prefixes.add(word.substring(0, i));
+            }
+        }
+    }
+
+    public Dictionary(HashSet<String> dictionary) {
+        this.dictionary = dictionary;
+        this.prefixes = new HashSet<>();
+        for (String word : dictionary) {
+            for (int i = 0; i < word.length(); i++) {
+                prefixes.add(word.substring(0, i));
+            }
+        }
+    }
 
     public boolean isPrefix(String prefix){
       return prefixes.contains(prefix);
@@ -15,21 +46,5 @@ public class Dictionary {
 
     public boolean isWord(String word) {
         return dictionary.contains(word);
-    }
-
-    public HashSet<String> getDictionary() {
-        return dictionary;
-    }
-
-    public void setDictionary(HashSet<String> dictionary) {
-        this.dictionary = dictionary;
-    }
-
-    public HashSet<String> getPrefixes() {
-        return prefixes;
-    }
-
-    public void setPrefixes(HashSet<String> prefixes) {
-        this.prefixes = prefixes;
     }
 }
--- assignment3/WordFinderTest.java
+++ assignment3/WordFinderTest.java
@@ -1,7 +1,9 @@
 package com.katermar.assignment3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * Created by katermar on 6/9/2017.
@@ -13,19 +15,23 @@ public class WordFinderTest {
                 {'A', 'A', 'R'},
                 {'T', 'C', 'D'}
         };
-        Dictionary dictionary = new Dictionary();
         String dictArray[] = {"CAR", "CARD", "CART", "CAT"};
-        String prefixArray[] = {"C", "CA", "CAR", "CARD", "CART", "CAT"};
+//        String prefixArray[] = {"C", "CA", "CAR", "CARD", "CART", "CAT"};
+        Dictionary dictionary = new Dictionary(dictArray);
         String expectedResultArray[] = {"CAR", "CARD", "CAT"};
-        dictionary.setDictionary(new HashSet<String>(Arrays.asList(dictArray)));
-        dictionary.setPrefixes(new HashSet<String>(Arrays.asList(prefixArray)));
 
         testWordFinder(new HashSet<String>(Arrays.asList(expectedResultArray)), dictionary, grid);
         testWordFinder(new HashSet<String>(), dictionary, grid);
+        char emptyGrid[][] = {{}, {}};
+        testWordFinder(new HashSet<String>(Arrays.asList(expectedResultArray)), dictionary, emptyGrid);
     }
 
     public static void testWordFinder(HashSet<String> expectedResult, Dictionary dictionary, char grid[][]) {
         WordFinder wordFinder = new WordFinder(dictionary);
+        if (wordFinder.findWords(grid) == null) {
+            System.out.println("No Words found <----- ERROR! Test FAILED");
+            return;
+        }
         System.out.print(wordFinder.findWords(grid).toString());
         if (wordFinder.findWords(grid).equals(expectedResult)) {
             System.out.println(" <----- Grats! Test PASSED");
